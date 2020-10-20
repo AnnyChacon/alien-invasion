ac9272675eaef77430e1cb3555f5d5e0f9d593be
Anny Chacon <anny.chacon@unet.edu.ve>
2020-10-19 20:51:28 -0400

    Layers with coordinates

--- index.js
+++ index.js
@@ -23,13 +23,54 @@ file.read('./Files/input.txt', function (err, data) {
                 ship.push(subLine);
             }
             numberLine += line[0];
+            let layers = getLayers(ship);
             ships.push({
                 id: i,
                 dx: line[0],
                 dy: line[1],
                 l: line[2],
                 ship: ship,
+                layers: layers,
             });
         }
     }
 })
+
+function getLayers(ship) {
+    let found = false;
+    let layers = [];
+    for (let i = 0; i < ship.length; i++) {
+        for (let j = 0; j < ship[i].length; j++) {
+            found = false;
+            if(ship[i][j] !== ' '){
+                layers.forEach(layout => {
+                    if(layout.letter === ship[i][j]){
+                        found = true;
+                        if(i < layout.xa){
+                            layout.xa = i;
+                        }
+                        if(i > layout.xb){
+                            layout.xb = i;
+                        }
+                        if(j < layout.ya){
+                            layout.ya = j;
+                        }
+                        if(j > layout.yb){
+                            layout.yb = j;
+                        }
+                    }
+                });
+                if(!found){
+                    layers.push({
+                        id: ship[i][j],
+                        xa: i,
+                        xb: i,
+                        ya: j,
+                        yb: j,
+                    })
+                }
+            }
+        }
+    }
+    return layers;
+}
